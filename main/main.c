92aff42df314148fc2b9f142c695a3534183e596
ryanphransa <ryanlimafranca@gmail.com>
2026-09-22 15:48:20 -0300

    update

--- main/main.c
+++ main/main.c
@@ -2,17 +2,15 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-float operands[100];
-char operators[100];
-
-float results[100];
-
 char *fakegetline(char string[]) {
     int i, c;
     i = 0;
     while((c = getchar()) != '\n') {
         if (c != ' ')
         string[i++] = c;
+        if (c == EOF) {
+            printf("FEIJAO\n");
+        }
     }
     string[i] = '\0';
 
@@ -22,7 +20,7 @@ char *fakegetline(char string[]) {
 int isoperator(char c) {
     static char operator = 0;
     if (c == '+' || c == '-' ||
-        c == '*' || c == '/') {
+        c == '*' || c == '/' ) {
         operator = c;
         return true;
     }
@@ -62,7 +60,7 @@ float operation(float op1, float op2, char operator) {
     }
 }
 
-void convert(char string[]) {
+void convert(char string[], char *operators, float *operands) {
     for (size_t i = 0; ; i++) {
         static size_t j = 0;
         static size_t k = 0;
@@ -97,25 +95,25 @@ void convert(char string[]) {
     }
 }
 
-void operation_order(void) {
+
+// Instead of global operators, pointers to the first element;
+float operation_order(char *operators, float *operands) {
+    float results[100] = {0};
     for (size_t i = 1; ; i++) {
-        static size_t j = 0;
-        static float result = 0;
-        float temp_result = 0;
-
-        printf("%c <- i=%d\n", operators[i], i);
-
+        printf("a");
         static int my_op = 0;
         static int last_op = 0;
 
-        if (pemdas(operators[i]) > pemdas(operators[my_op]))
+        if (pemdas(operators[i]) > pemdas(operators[my_op])) {
             my_op = i;
+        }
 
         if (operators[i] == '\0' && operators[my_op] != 0) {
-            // Break when don't find a operator that is,
-            // every element of operators is == 'X'.
-            if (operators[my_op] == 'X')
-                break;
+            // Break when every element of operators is == 'X'.
+            if (operators[my_op] == 'X') {
+                return results[last_op];
+            }
+
             printf("%c <- operator im using\n"
                    "%d <- last_op\n", operators[my_op], last_op);
 
@@ -125,43 +123,32 @@ void operation_order(void) {
                                            operators[my_op]);
             } else if (operators[my_op-1] == 'X' &&
                        operators[my_op+1] == 'X') {
-                // Actually i should identify who is the op1 and op2
-                // How?:
-                // Probably using only results[] instead of result and temp_result,
-                // now with results[my_op] is pretty easir to find op1 and op2,
-                // look:
                 results[my_op] = operation(results[my_op-1],
                                            results[my_op+1],
                                            operators[my_op]);
-                printf("a\n");
             } else if (last_op < my_op) {
-                if (last_op != my_op-1) {
+                if (last_op != my_op-1 && operators[my_op-1] != 'X') {
                     results[my_op] = operation(operands[my_op],
                                                operands[my_op+1],
                                                operators[my_op]);
-                    printf("b\n");
                 }
                 else {
                     results[my_op] = operation(results[last_op],
                                                operands[my_op+1],
                                                operators[my_op]);
-                    printf("d\n");
                 }
             } else if (last_op > my_op) {
-                if (last_op != my_op+1) {
+                if (last_op != my_op+1 && operators[my_op+1] != 'X') {
                     results[my_op] = operation(operands[my_op],
                                                operands[my_op+1],
                                                operators[my_op]);
-                    printf("c\n");
                 }
                 else {
                     results[my_op] = operation(results[last_op],
                                                operands[my_op], 
                                                operators[my_op]); 
-                    printf("e\n");
                 }
             }
-
 
             printf("%f <- result\n", results[my_op]);
             
@@ -175,18 +162,6 @@ void operation_order(void) {
             i = my_op = 0;
         }
     }
-}
-
-int main() {
-    char string[100];
-
-    fakegetline(string);
-
-    // Convert string chars in operands and operators
-    convert(string);
-
-    // Start the operations calling with PEMDAS order
-    operation_order();
 
     printf("Operands: \n");
     for(size_t i = 0; operands[i] != 0; i++)
@@ -202,6 +177,31 @@ int main() {
     printf("Results: \n");
     for(size_t i = 0; operators[i] != 0; i++) {
         printf("%f ", results[i]);
+    }
+}
+
+int main() {
+    char string[100];
+
+    float operands[100];
+    char operators[100];
+    while(1) { 
+        fakegetline(string);
+
+        // Convert string chars in operands and operators
+        convert(string, operators, operands);
+
+        // Start the operations calling with PEMDAS order
+        float result = operation_order(operators, operands);
+
+        printf("result is: %f\n", result);
+
+        //clear arrays
+        for (size_t i; i < 100; i++) {
+            string[i] = 0;
+            operands[i] = 0;
+            operators[i] = 0;
+        }
     }
 
     return 0;
